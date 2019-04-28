1c4cd3d762b0056e78fbfbf08cdbc573aef06e05
millanie <kymun@mymongo.20elwrigxkxupc0tn2mrcckheg.bx.internal.cloudapp.net>
2019-04-28 01:16:19 +0000

    initial commit

---
+++ scripts/scheduled_event_for_linux.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import json
+import urllib2
+import socket
+import sys
+
+metadata_url = "http://169.254.169.254/metadata/scheduledevents?api-version=2017-11-01"
+headers = "{Metadata:true}"
+this_host = socket.gethostname()
+
+def get_scheduled_events():
+   req = urllib2.Request(metadata_url)
+   req.add_header('Metadata', 'true')
+   resp = urllib2.urlopen(req)
+   data = json.loads(resp.read())
+   return data
+
+def handle_scheduled_events(data):
+    for evt in data['Events']:
+        eventid = evt['EventId']
+        status = evt['EventStatus']
+        resources = evt['Resources']
+        eventtype = evt['EventType']
+        resourcetype = evt['ResourceType']
+        notbefore = evt['NotBefore'].replace(" ","_")
+        if this_host in resources:
+            print "+ Scheduled Event. This host " + this_host + " is scheduled for " + eventtype + " not before " + notbefore
+            # Add logic for handling events here
+
+
+def main():
+   data = get_scheduled_events()
+   handle_scheduled_events(data)
+
+if __name__ == '__main__':
+  main()
+  sys.exit(0)
+
